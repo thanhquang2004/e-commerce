08c9e531f96dd4e391f76a91882a3f8dcbe19fcd
phanlong264 <phanlong204dp@gmail.com>
2024-08-13 11:15:40 +0700

    about

--- html/taycamXbox.php
+++ html/taycamXbox.php
@@ -5,33 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tay Cầm Chơi Game PC</title>
-    <link rel="stylesheet" href="/css/xbox.css">
+    <link rel="stylesheet" href="../css/xbox.css">
 </head>
 
 <body>
     <div id="wrapper">
         <header id="header">
             <div class="logo">
-                <img src="/img/logo.png" alt="Logo">
+                <img src="../img/logo.png" alt="Logo">
             </div>
             <nav class="main-nav">
-                <a href="/index.html">Trang chủ</a>
-                <a href="/html/contact.html">Liên Hệ</a>
-                <a href="/html/about.html">About</a>
-                <a href="/login.html">Đăng Ký</a>
+                <a href="../index.php">Trang chủ</a>
+                <a href="../html/contact.php">Liên Hệ</a>
+                <a href="../html/about.php">About</a>
+                <a href="../login.php">Đăng Ký</a>
             </nav>
             <div id="actions">
                 <div class="search">
                     <input type="text" class="searchTerm" placeholder="What are you looking for?">
                     <button type="submit" class="searchButton">
-                        <i class="fa fa-search"><img src="/img/Search_button.png" alt="Search"></i>
+                        <i class="fa fa-search"><img src="../img/Search_button.png" alt="Search"></i>
                     </button>
                 </div>
                 <div class="item">
-                    <a href="/html/yeuthich.html"><img src="/img/heart_icon.png" alt="User Icon"></a>
+                    <a href="../html/yeuthich.php"><img src="../img/heart_icon.png" alt="User Icon"></a>
                 </div>
                 <div class="item_cart">
-                    <a href="/html/cart.html"><img src="/img/cart_icon.png" alt="Cart Icon"></a>
+                    <a href="../html/cart.php"><img src="../img/cart_icon.png" alt="Cart Icon"></a>
                 </div>
             </div>
         </header>
@@ -48,7 +48,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI005.jpg" alt="">
+                                <img src="../img/Xbox/GPMI005.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -63,7 +63,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI031.jpg" alt="">
+                                <img src="../img/Xbox/GPMI031.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -78,7 +78,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI032.jpg" alt="">
+                                <img src="../img/Xbox/GPMI032.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -93,7 +93,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI033.jpg" alt="">
+                                <img src="../img/Xbox/GPMI033.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -108,7 +108,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI035.jpg" alt="">
+                                <img src="../img/Xbox/GPMI035.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -123,7 +123,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI036.jpg" alt="">
+                                <img src="../img/Xbox/GPMI036.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -138,7 +138,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI038.jpg" alt="">
+                                <img src="../img/Xbox/GPMI038.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -153,7 +153,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI040.jpg" alt="">
+                                <img src="../img/Xbox/GPMI040.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -169,7 +169,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI041.jpg" alt="">
+                                <img src="../img/Xbox/GPMI041.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -184,7 +184,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI043.jpg" alt="">
+                                <img src="../img/Xbox/GPMI043.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -199,7 +199,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI059.jpg" alt="">
+                                <img src="../img/Xbox/GPMI059.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -215,7 +215,7 @@
                     <div class="product-item">
                         <div class="product-top">
                             <a href="" class="product-thumb">
-                                <img src="/img/Xbox/GPMI065.jpg" alt="">
+                                <img src="../img/Xbox/GPMI065.jpg" alt="">
                             </a>
                             <a href="" class="addtocart">Thêm Vào Giỏ Hàng</a>
                         </div>
@@ -236,36 +236,36 @@
                 <address>
                     Địa Chỉ : <br>
                     Số điện thoại :<br>
-                    Email :
+                    Email : 
                     <ul class="social-links">
-                        <li><a href="#"><img src="/img/Icon-Facebook.png" alt="Facebook"></a></li>
-                        <li><a href="#"><img src="/img/Icon-Twitter.png" alt="Twitter"></a></li>
-                        <li><a href="#"><img src="/img/icon-instagram.png" alt="Instagram"></a></li>
+                        <li><a href="#"><img src="../img/Icon-Facebook.png" alt="Facebook"></a></li>
+                        <li><a href="#"><img src="../img/Icon-Twitter.png" alt="Twitter"></a></li>
+                        <li><a href="#"><img src="../img/icon-instagram.png" alt="Instagram"></a></li>
                     </ul>
                 </address>
             </div>
             <div class="account">
                 <h3>Thông Tin</h3>
                 <ul>
-                    <li><a href="/myaccount">Tài Khoản</a></li>
-                    <li><a href="/login.html">Login / Register</a></li>
-                    <li><a href="/html/cart.html">Giỏ Hàng</a></li>
-                    <li><a href="/index.html">Cửa Hàng</a></li>
+                    <li><a href="../myaccount">Tài Khoản</a></li>
+                    <li><a href="../login.php">Login / Register</a></li>
+                    <li><a href="../html/cart.php">Giỏ Hàng</a></li>
+                    <li><a href="../index.php">Cửa Hàng</a></li>
                 </ul>
             </div>
             <div class="quick-link">
                 <h3>Chính sách</h3>
                 <ul>
-                    <li><a href="/html/privacy.html">Chính sách bảo mật</a></li>
-                    <li><a href="/html/terms.html">Điều khoản sử dụng</a></li>
-                    <li><a href="/html/faq.html">Câu hỏi thường gặp</a></li>
-                    <li><a href="/html/contact.html">Liên Hệ</a></li>
+                    <li><a href="../html/privacy.php">Chính sách bảo mật</a></li>
+                    <li><a href="../html/terms.php">Điều khoản sử dụng</a></li>
+                    <li><a href="../html/faq.php">Câu hỏi thường gặp</a></li>
+                    <li><a href="../html/contact.php">Liên Hệ</a></li>
                 </ul>
             </div>
             <div class="exclusive">
-                <h4>.... Shop game chuyên kinh doanh PS5, <br />
-                    PS4, PS3, PS2, Xbox 360, Xbox One, Nintendo Wii, Nintendo Switch...
-                    và phụ kiện chính hãng. Chúng tôi tự tin mang đến những sản phẩm, <br />
+                <h4>.... Shop game chuyên kinh doanh PS5, <br/>
+                    PS4, PS3, PS2, Xbox 360, Xbox One, Nintendo Wii, Nintendo Switch... 
+                    và phụ kiện chính hãng. Chúng tôi tự tin mang đến những sản phẩm, <br/>
                     dịch vụ chất lượng với giá thành phù hợp với mọi game thủ.</h4>
             </div>
         </div>
@@ -273,7 +273,8 @@
             <p>&copy;</p>
         </div>
     </footer>
-
+    
+    
 </body>
 
 </html>
